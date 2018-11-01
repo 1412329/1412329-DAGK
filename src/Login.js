0db8b8e496a6e9f6b674c4a3010328efaec4ea02
MinhVu131 <1412329@student.hcmus.edu.vn>
2018-11-01 18:38:15 +0700

    1412329 - Create Sample App

---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Login = props => (
+    <div className="login">
+      <p>Click button below to sign in!</p>
+      <button className="github">Log In With Gitub</button>
+      <button className="gmail">Log In With Facebook</button>
+    </div>
+  );
+
+  export default Login;
